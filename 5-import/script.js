491b794fab58f94f4219dff001a2da4b3755a3d0
ced <cedric.milius@yahoo.fr>
2019-04-24 17:14:58 +0200

    Import / Export section 2 : Exporter les fonctions du fichier math.js pour pouvoir les utiliser dans le script principal (script.js)

--- 5-import/script.js
+++ 5-import/script.js
@@ -2,8 +2,11 @@
 
 import {hello} from './functions.js' //import une fonction se trouvant dans le repertoire functions.js
 import {showError} from './functions.js'
-import {default as anonyme} from './functions.js'
-anonyme()
+//import {default as anonyme} from './functions.js'
+import * as math from './math.js'
+
+
+//anonyme()
 
 showError('message d\'erreur')
 
@@ -14,3 +17,9 @@ hello()
 
 // 5-2 Export
 const [a, b] = [7, 9]
+
+console.log(math.sum(a, b))
+console.log(math.sub(a, b))
+console.log(math.multiply(a, b))
+console.log(math.divide(a, b))
+console.log(math.pow(a, b))

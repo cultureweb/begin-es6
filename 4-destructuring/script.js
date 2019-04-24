5d2117bb7465921b225299f08ed87e15420dd10b
ced <cedric.milius@yahoo.fr>
2019-04-24 15:18:14 +0200

    Destructuring Section 1

--- 4-destructuring/script.js
+++ 4-destructuring/script.js
@@ -1,13 +1,20 @@
 //4.1 Découverte
+/*L'affectation par décomposition (destructuring en anglais) est une expression JavaScript qui permet d'extraire (unpack en anglais) des données d'un tableau ou d'un objet grâce à une syntaxe dont la forme ressemble à la structure du tableau ou de l'objet. */
 
-const [a, b] = [1, 2, 3, 4]
+
+const [a, b] = [1, 2, 3, 4]  // On utilise l'affectation par décomposition
 console.log(a)
 console.log(b)
 
-const {first, last} = {first: 'Paul', last: 'Henta', age: 35}
+const {first, last, age} = {first: 'Paul', last: 'Henta', age: 35}
 console.log(first)
 console.log(last)
+console.log(age)
+
+
+
 
 //4.2 Application
 
 console.log(data)
+

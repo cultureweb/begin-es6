3025284cd379d70977825dbb015184b7c6a438ec
ced <cedric.milius@yahoo.fr>
2019-04-24 17:03:22 +0200

    Import / Export section 1 : Importer et utiliser la fonction anonyme définie à la fin du fichier

--- 5-import/script.js
+++ 5-import/script.js
@@ -2,11 +2,15 @@
 
 import {hello} from './functions.js' //import une fonction se trouvant dans le repertoire functions.js
 import {showError} from './functions.js'
-
+import {default as anonyme} from './functions.js'
+anonyme()
 
 showError('message d\'erreur')
 
 hello()
+
+
+
 
 // 5-2 Export
 const [a, b] = [7, 9]
